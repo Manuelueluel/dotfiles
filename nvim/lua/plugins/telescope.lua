f190e45b7f3b27a769cade8242791866070897bf
Manuelueluel <mondin.manuel@gmail.com>
2025-07-17 16:27:15 +0200

    zk.nvim keymapping

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -85,6 +85,10 @@ return {
       vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
       vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
 
+      -- Used even for [[wikilinks]]
+      vim.keymap.set('n', 'gd', builtin.lsp_definitions, { desc = '[G]oto [Definition]' })
+      vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, { desc = '[G]oto [D]eclaration' })
+
       -- Slightly advanced example of overriding default behavior and theme
       vim.keymap.set('n', '<leader>/', function()
         -- You can pass additional configuration to Telescope to change the theme, layout, etc.
